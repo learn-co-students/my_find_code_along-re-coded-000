ff4982161dcaee6898f07647490de568b28ca81b
nichervan brhik <nichervan.brhik@gmail.com>
2017-01-08 12:14:13 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,12 @@
 require 'pry'
-
+a=[1,2,3]
 def my_find(collection)
   i = 0
-  while i < collection.length
+  while i < collection.size
     return collection[i] if yield(collection[i])
-    i = i + 1
+    i+= 1
   end
+end
+my_find(a) do |item|
+  item.even?
 end
